adb68353e5d2f58c959c7099084e6ad0de9eef8a
vlaier <piotrzielu.1996@gmail.com>
2023-02-06 15:46:43 +0100

    change config file

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
-    domains: ["localhost"],
+    domains: ["localhost, serwer2252942.home.pl/autoinstalator/wordpress"],
   },
 };
 
