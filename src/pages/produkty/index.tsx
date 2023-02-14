d4a43aec00bbc0f8a8a77fee247cb4fa2b0661d7
vlaier <piotrzielu.1996@gmail.com>
2023-02-14 16:15:36 +0100

    Add fallback for products

--- src/pages/produkty/index.tsx
+++ src/pages/produkty/index.tsx
@@ -1,16 +1,48 @@
 import { GetProductsFeedDocument } from "@/lib/codegenOutput/graphql";
-import useSWR from "swr";
+import useSWR, { unstable_serialize } from "swr";
 import { graphqlDataToProductsData } from "@/lib/utils";
 import { IProductGeneral } from "@/components/products";
 import { ProductsGrid } from "@/components/products/display/ProductsGrid";
+import { graphqlClient } from "@/lib/client";
+import { InferGetStaticPropsType, GetStaticPropsContext } from "next";
+import { fetcher } from "../../lib/client";
+import request from "graphql-request";
+const ProductsPage = ({
+  fallback,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const endpoint =
+    "https://serwer2252942.home.pl/autoinstalator/wordpress/graphql";
 
-const ProductsPage = () => {
-  const { data, isLoading, error } = useSWR(GetProductsFeedDocument);
-  if (isLoading) return <div>Ładowanie...</div>;
+  const { data, isLoading, error } = useSWR(
+    [GetProductsFeedDocument],
+    ([query]) => fetcher(query),
+    { fallback }
+  );
+  if (!data) return <div>Ładowanie...</div>;
   if (error) return <div>Błąd</div>;
   const products = graphqlDataToProductsData(data);
   return <ProductsGrid products={products as IProductGeneral[]} />;
 };
 
 export default ProductsPage;
-// GraphQL get static props
+export const getStaticProps = async () => {
+  const endpoint =
+    "https://serwer2252942.home.pl/autoinstalator/wordpress/graphql";
+  const data = await graphqlClient
+    .request(GetProductsFeedDocument, {})
+    .then((data) => data);
+
+  if (!data || !data.products) {
+    return {
+      props: {},
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      fallback: {
+        [unstable_serialize([GetProductsFeedDocument])]: data,
+      },
+    },
+  };
+};
